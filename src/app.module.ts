fa3d3dd98671b0d06497d4c9f64cfd5699076257
Matheus S Alves <matheus.souza.alves@gmail.com>
2026-01-23 16:20:37 -0300

    feat(nestjs): setup ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 🔑 não precisa importar em todos os módulos
+      envFilePath: ['.env.local', `.env.${process.env.NODE_ENV}`, '.env'],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        DATABASE_URL: Joi.string().uri().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().port().required(),
+        NEST_PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NEST_PORT ?? 3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>('NEST_PORT', 3000);
+
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+  process.exit(1);
+});
