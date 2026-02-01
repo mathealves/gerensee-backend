8e7dbfd025a3097a38cbb2f1f82b89434e2ecd38
Matheus S Alves <matheus.souza.alves@gmail.com>
2026-02-01 20:44:50 -0300

    feat: global exception filter and validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { DatabaseModule } from './core/database/database.module';
+import { AuthModule } from './core/auth/auth.module';
+import { HttpExceptionFilter } from './core/common/filters/http-exception.filter';
+import { ValidationPipe } from './core/common/pipes/validation.pipe';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
 import * as Joi from 'joi';
 
 @Module({
-  controllers: [],
-  providers: [],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -21,10 +23,26 @@ import * as Joi from 'joi';
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_PORT: Joi.number().port().required(),
         NEST_PORT: Joi.number().default(3000),
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_ACCESS_EXPIRATION: Joi.string().default('15m'),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_REFRESH_EXPIRATION: Joi.string().default('30d'),
       }),
     }),
     DatabaseModule,
+    AuthModule,
     OrganizationsModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class AppModule {}
